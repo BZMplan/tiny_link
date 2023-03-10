74b90d73caf7ca084b6bdda1517c890e99297938
bzmplan <15249938392@163.com>
2023-03-10 18:40:17 +0800

    final commit

--- src/api/links.rs
+++ src/api/links.rs
@@ -1,7 +1,11 @@
+use std::path;
+
 use actix_web::{web::Json, Responder};
 use serde::{Deserialize,Serialize};
 use nanoid::nanoid;
 use actix_web::{post,get,web::{Path},HttpResponse,http::header};
+
+use crate::api::ApiResult;
 
 #[derive(Deserialize,Serialize,Debug,Clone)]
 struct Links{
@@ -29,11 +33,25 @@ impl ApiAddLink {
 async fn create_link(link:Json<ApiAddLink>)-> impl Responder{
     let new_link = link.0.to_new_link();
     let new_code = new_link.tiny_code.clone();
-    Json(new_code)
+    Json(ApiResult::success(Some(new_code)))
 }
 
 #[get("/{code}")]
-async fn get_from_link()-> impl Responder{
+async fn get_from_link(path:Path<String>)-> impl Responder{
+    let code = path.into_inner();
+    if code=="test"{
+        let api_result:ApiResult<String> = ApiResult::error("404".to_string());
+        return HttpResponse::Ok().json(api_result);
+    }
+    
     let url = "http://baidu.com";
     HttpResponse::Found().append_header((header::LOCATION,url)).finish()
+}
+
+#[post("/links")]
+async fn get_all_links()-> impl Responder{
+    let mut links:Vec<Links> = Vec::new();
+    links.push(Links{tiny_code:String::from("111"),origin_url:String::from("http://baidu.com")});
+    links.push(Links{tiny_code:String::from("111"),origin_url:String::from("http://bilibili.com")});
+    Json(ApiResult::success(Some(links)))
 }
